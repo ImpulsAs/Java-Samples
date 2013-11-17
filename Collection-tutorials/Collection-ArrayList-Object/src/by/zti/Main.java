275e636886143f55a694a8f10308097d0f02d3fc
Cvazer <zhirnitoni@gmail.com>
2013-11-17 03:17:46 +0300

    Remade tutorial

---
+++ Collection-tutorials/Collection-ArrayList-Object/src/by/zti/Main.java
@@ -0,0 +1,19 @@
+package by.zti;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+	private static ArrayList<Mobile> list = new ArrayList<Mobile>();
+	private static Random r = new Random();
+
+	public static void main(String[] args) {
+		for(int i=0;i<5000000;i++){
+			list.add(new Mobile(r.nextInt(9999999),"abc"+r.nextInt(100)));
+		}
+		for(Mobile m: list){
+			System.out.println(m.getNumber()+" "+m.getName());
+		}
+	}
+
+}
