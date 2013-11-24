ddd67c7bed505ffec5bdf80a4c49149c9e5eb5bb
Cvazer <zhirnitoni@gmail.com>
2013-11-24 06:01:22 +0400

    add jar files

--- Collection-tutorials/Collection-ArrayList-Object/src/by/zti/Main.java
+++ Collection-tutorials/Collection-ArrayList-Object/src/by/zti/Main.java
@@ -6,7 +6,6 @@ import java.util.Random;
 public class Main {
 	private static ArrayList<Mobile> list = new ArrayList<Mobile>();
 	private static Random r = new Random();
-
 	public static void main(String[] args) {
 		for(int i=0;i<5;i++){
 			list.add(new Mobile(r.nextInt(9999999),"abc"+r.nextInt(100)));
