7675e4ac8d96743d08e071a487298ba772944e45
Cvazer <zhirnitoni@gmail.com>
2013-11-24 06:12:41 +0400

    Исправил недочет при десереализации

--- Collection-tutorials/Collection-ArrayList-Object/src/by/zti/Main.java
+++ Collection-tutorials/Collection-ArrayList-Object/src/by/zti/Main.java
@@ -6,8 +6,10 @@ import java.util.Random;
 public class Main {
 	private static ArrayList<Mobile> list = new ArrayList<Mobile>();
 	private static Random r = new Random();
+	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		SerializationManager.deSerializeData("mobile-list", "ser", "");
+		list = (ArrayList<Mobile>) SerializationManager.deSerializeData("mobile-list", "ser", "");
 //		for(int i=0;i<5;i++){
 //			list.add(new Mobile(r.nextInt(9999999),"abc"+r.nextInt(100)));
 //		}
